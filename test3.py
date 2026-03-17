f27bfb967506729b9141213c319097fdba723e1b
surekha3 <rekhasundar3@gmail.com>
2026-03-17 10:16:40 +0530

    fixed python code

--- test3.py
+++ test3.py
@@ -4,5 +4,13 @@ def func(n):
       print(" ", end="")
     print(i*"* ", end="")
     print()
-n = int(input("enter natural number as user input: "))
+user_input = input("enter natural number as user input: ")
+if user_input == "":
+  n = 5
+else:
+  try:
+    n = int(user_input)
+  except ValueError:
+    print("Invalid input! Using default value as 5")
+    n = 5
 func(n)
